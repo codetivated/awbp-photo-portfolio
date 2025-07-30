c36ff61f254d2dde3195c51c1c839f5fc352718e
codetivated <melisa@heying.us>
2025-07-30 09:37:36 -0500

    add navbar and footer & organize routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryGridComponent } from './components/gallery-grid/gallery-grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'gallery', component: GalleryGridComponent }
+];
