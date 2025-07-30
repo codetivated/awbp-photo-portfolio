0b311f6641578825b96896f5a36192e80be018df
codetivated <melisa@heying.us>
2025-07-30 09:14:03 -0500

    create files, add custom css & update favicon

---
+++ src/app/components/gallery-grid/gallery-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-grid',
+  imports: [],
+  templateUrl: './gallery-grid.component.html',
+  styleUrl: './gallery-grid.component.css'
+})
+export class GalleryGridComponent {
+
+}
